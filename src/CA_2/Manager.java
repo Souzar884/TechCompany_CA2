801de8a511685cba5624288fecbd6603107253a5
Souzar884 <souza.rogerde@gmail.com>
2025-05-02 15:15:14 +0100

    Second Commit: Department,DepartmentName,Manager,ManagerType

---
+++ src/CA_2/Manager.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CA_2;
+
+/**
+ *
+ * @author roger
+ */
+class Manager {
+    private ManagerType type;
+
+    public Manager(ManagerType type) {
+        this.type = type;
+    }
+
+    public ManagerType getType() {
+        return type;
+    }
+}
---
+++ src/CA_2/ManagerType.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CA_2;
+
+/**
+ *
+ * @author roger
+ */
+enum ManagerType {
+    HEAD_MANAGER, ASSISTANT_MANAGER, TEAM_LEAD
+}
