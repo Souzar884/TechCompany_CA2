bbc6446300784bf41733d47da5e24e4c51ef2210
Souzar884 <souza.rogerde@gmail.com>
2025-05-06 00:03:59 +0100

    Commit: EmployeeManager(1.0)

--- src/CA_2/EmployeeManager.java
+++ src/CA_2/EmployeeManager.java
@@ -8,6 +8,30 @@ package CA_2;
  *
  * @author roger
  */
-public class EmployeeManager {
-    
-}
+import java.util.*;
+import java.io.*;
+
+class EmployeeManager {
+    private List<Employee> employees = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
+    private Random random = new Random();
+
+    public void run() {
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("\nSelect an option:");
+            for (MenuOption option : MenuOption.values()) {
+                System.out.println(option.ordinal() + 1 + ". " + option);
+            }
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (MenuOption.values()[choice - 1]) {
+                case SORT -> sortEmployees();
+                case SEARCH -> searchEmployee();
+                case ADD -> addEmployee();
+                case GENERATE -> generateRandomEmployees();
+                case EXIT -> exit = true;
+            }
+        }
+    }
